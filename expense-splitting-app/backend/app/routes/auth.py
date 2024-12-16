c59407a30c2e6dc2d5733520cbd951382f22d5da
Jusca Molaiwa <jusca.mol@gmail.com>
2024-12-17 00:18:03 +0200

    added registration form fields validations

--- expense-splitting-app/backend/app/routes/auth.py
+++ expense-splitting-app/backend/app/routes/auth.py
@@ -2,6 +2,7 @@ from functools import wraps
 from flask import Blueprint, request, jsonify, current_app # type: ignore
 from ..services.user_service import UserService  # Import the UserService
 import jwt # type: ignore
+from ..models import User
 
 bp = Blueprint('auth', __name__)
 
@@ -11,7 +12,7 @@ def register():
     data = request.get_json()
 
     # Required fields for registration
-    required_fields = {'username', 'email', 'password'}
+    required_fields = {'username', 'email', 'password', 'full_name'}
 
     # Find missing fields
     missing_fields = required_fields - data.keys() if data else required_fields
@@ -50,3 +51,23 @@ def login():
         token = UserService.generate_jwt_token(user.id, user.role, remember_me)
         return jsonify({'token': token}), 200
     return jsonify({'error': 'Invalid credentials'}), 401
+
+
+@bp.route('/api/check-email', methods=['POST'])
+def check_email():
+    email = request.json.get('email')
+    if not email:
+        return jsonify({'message': 'Email is required'}), 400
+
+    if UserService.validate_email(email):
+        return jsonify({'isRegistered': True}), 200
+
+    return jsonify({'isRegistered': False}), 200
+
+@bp.route('/api/check-username', methods=['POST'])
+def check_username():
+    data = request.get_json()
+    username = data.get('username')
+    if User.query.filter_by(username=username).first():
+        return jsonify({"isTaken": True, "message": "Username is already taken"}), 409
+    return jsonify({"isTaken": False, "message": "Username is available"}), 200
