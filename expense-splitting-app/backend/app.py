7cb589c880ac8cc047733c3412e74ac7f4017ac7
Jusca Molaiwa <jusca.molaiwa@entelect.co.zagit config --global user.email jusca.molaiwa@entelect.co.za>
2024-12-16 17:46:07 +0200

    Remove .pyc files from version control

--- expense-splitting-app/backend/app.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask # type: ignore
-from app import create_app
-
-# Create the Flask application
-app = create_app()
-
-if __name__ == '__main__':
-    app.run(debug=True, port=5000)
